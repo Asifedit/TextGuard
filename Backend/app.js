2bdd0175a8826f67ee2bae6fd449a70eced48f27
Asif <152059881+Asifedit@users.noreply.github.com>
2024-07-22 10:41:15 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,7 @@ require("dotenv").config();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -31,10 +32,10 @@ app.use("/api/users", userRoutes);
 mongoose
     .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
     })
     .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.log(err));
+    .catch((err) => console.log("Error connecting to MongoDB:", err));
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
